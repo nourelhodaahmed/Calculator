d53bdec0dddccf73503a934aece8fddae25e2519
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-09-01 19:10:32 +0300

    fix: issues

--- app/src/main/java/com/android/calculator/CalculatorViewModel.kt
+++ app/src/main/java/com/android/calculator/CalculatorViewModel.kt
@@ -1,5 +1,6 @@
 package com.android.calculator
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -10,6 +11,11 @@ class CalculatorViewModel() : ViewModel() {
     val state = _state.asStateFlow()
 
     fun onNumberClicked(number: String) {
+        if (
+            _state.value.operators.isNotEmpty() &&
+            _state.value.operators.last() == Operator.MOD
+        ) return
+
         _state.update {
             it.copy(
                 currentNumber = if (_state.value.currentNumber == "0" && number != ".") number
@@ -32,6 +38,7 @@ class CalculatorViewModel() : ViewModel() {
     fun onOperatorClicked(operator: Operator) {
         if (
             _state.value.operators.isNotEmpty() &&
+            _state.value.operators.last() != Operator.MOD &&
             _state.value.operators.size == _state.value.numbers.size + 1
         ) {
             return
@@ -45,8 +52,15 @@ class CalculatorViewModel() : ViewModel() {
                     append(" ")
                     append(operator.toEquation())
                 },
-                numbers = _state.value.numbers + _state.value.currentNumber,
-                currentNumber = "0",
+                numbers =
+//                    if (
+//                    _state.value.operators.isNotEmpty() &&
+//                    _state.value.operators.last() != Operator.MOD
+//                    )
+//                    _state.value.numbers + _state.value.currentNumber
+//                else
+                    _state.value.numbers,
+                currentNumber = if (operator == Operator.MOD) "" else "0",
                 operators = _state.value.operators + operator
             )
         }
@@ -65,28 +79,56 @@ class CalculatorViewModel() : ViewModel() {
     }
 
     fun deleteLast() {
-        if (_state.value.currentNumber == "0") return
-        val newNum = _state.value.currentNumber.dropLast(1)
-        _state.update { it.copy(currentNumber = if (newNum.isEmpty()) "0" else newNum) }
+        if (_state.value.currentNumber == "0" && _state.value.operators.isEmpty()) return
+        else if (
+            (_state.value.currentNumber == "0" || _state.value.currentNumber == "")&&
+            _state.value.operators.isNotEmpty())
+        {
+            val lastNumber = _state.value.currentEquation
+                .split(" ")
+                .dropLast(1)
+                .last()
+            _state.update {
+                it.copy(
+                    currentEquation = _state.value.currentEquation
+                        .split(" ")
+                        .dropLast(2)
+                        .joinToString(" "),
+                    currentNumber = lastNumber,
+                    numbers = _state.value.numbers.dropLast(1),
+                    operators = _state.value.operators.dropLast(1)
+                )
+            }
+        }
+        else {
+            val newNum = _state.value.currentNumber.dropLast(1)
+            _state.update { it.copy(currentNumber = if (newNum.isEmpty()) "0" else newNum) }
+        }
     }
 
     fun onEqualButtonClicked() {
         if (_state.value.currentNumber == UNDEFINED) return
-        _state.update { it.copy(numbers = _state.value.numbers + _state.value.currentNumber) }
-        if (_state.value.operators.size == _state.value.numbers.size) {
-            return
-        }
+        val operatorsSize =
+            if (_state.value.operators.contains(Operator.MOD))
+                _state.value.operators.size - 1
+            else
+                _state.value.operators.size
 
-        val res = calculateResult()
+        Log.d("operatorsSize", operatorsSize.toString())
+        Log.d("_state.value.numbers.size", _state.value.numbers.size.toString())
+        if (operatorsSize == _state.value.numbers.size) {
+            _state.update { it.copy(numbers = _state.value.numbers + _state.value.currentNumber) }
+            val res = calculateResult()
 
-        _state.update {
-            it.copy(
-                lastOperation = _state.value.currentEquation + " " + _state.value.currentNumber,
-                currentEquation = "",
-                currentNumber = res,
-                numbers = listOf(),
-                operators = listOf()
-            )
+            _state.update {
+                it.copy(
+                    lastOperation = _state.value.currentEquation + " " + _state.value.currentNumber,
+                    currentEquation = "",
+                    currentNumber = res,
+                    numbers = listOf(),
+                    operators = listOf()
+                )
+            }
         }
     }
 
@@ -98,15 +140,16 @@ class CalculatorViewModel() : ViewModel() {
         if (numbers.isEmpty()) return "0"
 
         var res = numbers[0].toFloat()
-        for (i in 0 until numbers.size - 1) {
+        for (i in 0 until operators.size) {
             when (operators[i]) {
                 Operator.DIVIDER -> {
                     if (numbers[i + 1].toFloat() == 0f) return UNDEFINED
                     res /= numbers[i + 1].toFloat()
                 }
+
                 Operator.MULTIPLY -> res *= numbers[i + 1].toFloat()
                 Operator.ADDITION -> res += numbers[i + 1].toFloat()
-                Operator.MOD -> res %= numbers[i + 1].toFloat()
+                Operator.MOD -> res /= 100
                 Operator.MINUS -> res -= numbers[i + 1].toFloat()
             }
         }
@@ -132,7 +175,7 @@ class CalculatorViewModel() : ViewModel() {
         }
     }
 
-    private companion object{
+    private companion object {
         const val UNDEFINED = "Undefined"
     }
 }
